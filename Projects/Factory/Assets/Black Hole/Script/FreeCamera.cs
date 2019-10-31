b7cd216401f3f9c0a4d9136766d15efa22039140
CharlesYWL <34050457+CharlesYWL@users.noreply.github.com>
2019-10-31 13:55:49 -0700

    Add blackhole assets and ground

---
+++ Projects/Factory/Assets/Black Hole/Script/FreeCamera.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class FreeCamera : MonoBehaviour
+{
+    public float m_MoveSpeed = 0f;
+    public float m_RotateSpeed = 0f;
+    public KeyCode m_ForwardButton = KeyCode.W;
+    public KeyCode m_BackwardButton = KeyCode.S;
+    public KeyCode m_RightButton = KeyCode.D;
+    public KeyCode m_LeftButton = KeyCode.A;
+    public KeyCode m_UpButton = KeyCode.Q;
+    public KeyCode m_DownButton = KeyCode.E;
+
+	void Update ()
+    {
+        // translation
+        {
+            Vector3 dir = Vector3.zero;
+			Move (m_ForwardButton, ref dir, transform.forward);
+			Move (m_BackwardButton, ref dir, -transform.forward);
+			Move (m_RightButton, ref dir, transform.right);
+			Move (m_LeftButton, ref dir, -transform.right);
+			Move (m_UpButton, ref dir, transform.up);
+			Move (m_DownButton, ref dir, -transform.up);
+			transform.position += dir * m_MoveSpeed * Time.deltaTime;
+        }
+        // rotation
+        {
+            if (Input.GetMouseButton (0))
+            {
+                Vector3 eulerAngles = transform.eulerAngles;
+				eulerAngles.x += -Input.GetAxis("Mouse Y") * 359f * m_RotateSpeed;
+				eulerAngles.y += Input.GetAxis("Mouse X") * 359f * m_RotateSpeed;
+                transform.eulerAngles = eulerAngles;
+            }
+        }
+    }
+    void Move (KeyCode key, ref Vector3 moveTo, Vector3 dir)
+    {
+        if (Input.GetKey (key))
+			moveTo = dir;
+    }
+}
---
+++ Projects/Factory/Assets/Black Hole/Shader/Black Hole.shader
@@ -0,0 +1,41 @@
+﻿Shader "Black Hole" {
+	Properties {
+		_MainTex ("Main", 2D) = "white" {}
+		_Center ("Center", Vector) = (0.5, 0.5, 0, 0)
+		_Distortion ("Distortion", Float) = -2
+		_DarkRange ("Dark Range", Float) = 0.1
+	}
+	CGINCLUDE
+		#include "UnityCG.cginc"
+		sampler2D _MainTex;
+		float2 _Center;
+		float _Distortion, _DarkRange;
+		float4 frag (v2f_img i) : SV_TARGET
+		{
+			float2 uv = i.uv;
+			float2 center = _Center * _ScreenParams.xy;
+			float2 uvpixel = uv * _ScreenParams.xy;
+			float dist = distance(center, uvpixel);
+
+			float2 warp = normalize(_Center - uv) * pow(dist, _Distortion) * 30.0;
+			warp.y = -warp.y;
+			uv = uv + warp;
+	
+			float light = saturate(_DarkRange * dist - 1.5);
+	
+			return tex2D(_MainTex, uv) * light;
+			
+		}
+	ENDCG
+	SubShader {
+		ZTest Off Cull Off ZWrite Off Blend Off
+	  	Fog { Mode off }
+		Pass {
+			CGPROGRAM
+			#pragma vertex vert_img
+			#pragma fragment frag
+			ENDCG
+		}
+	}
+	FallBack Off
+}
