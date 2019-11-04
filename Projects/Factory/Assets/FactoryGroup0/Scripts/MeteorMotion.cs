51baad3cae8dfe5b2556561497979f5696ade721
Aligulac <yfqyang@ucdavis.edu>
2019-11-04 11:38:47 -0800

    Update MeteorMotion.cs

--- Projects/Factory/Assets/FactoryGroup0/Scripts/MeteorMotion.cs
+++ Projects/Factory/Assets/FactoryGroup0/Scripts/MeteorMotion.cs
@@ -6,17 +6,27 @@ public class MeteorMotion : MonoBehaviour
 {
     private bool firsthit = false;
     [SerializeField] private GameObject prefab;
+    private Rigidbody Meteor;
+    private Vector3 Force;
 
+    void Start()
+    {
+        Meteor = GetComponent<Rigidbody>();
+        Force = new Vector3(Random.Range(-50,50), Random.Range(-50, -20), Random.Range(-50,50));
+    }
 
-
+    void FixedUpdate()
+    {
+        Meteor.AddForce(Force);
+    }
 
     void OnCollisionEnter(Collision col)
     {
         if (firsthit == true)
         {
             Debug.Log(gameObject.name + " was Collided with" + col.gameObject.name);
+            Instantiate(this.prefab, this.gameObject.transform.position, Quaternion.identity);
             Destroy(this.gameObject);
-            Instantiate(this.prefab);
         }
 
         firsthit = true;
