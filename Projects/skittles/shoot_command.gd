cce95418f8c27f7c4ab505bb4e175eb8d784d97e
AronMurai <87220403+AronMurai@users.noreply.github.com>
2025-09-29 21:49:07 -0700

    reverted instantiation error

--- Projects/skittles/shoot_command.gd
+++ Projects/skittles/shoot_command.gd
@@ -1,10 +1,10 @@
 class_name ShootCommand
 extends PlayerCommand
 
-@export var _projectile_scene : PackedScene
+var _projectile_scene = preload("res://projectile.tscn")
 
 func execute(player : Player) -> bool:
-	var projectile:Projectile = _projectile_scene.instantiate()
+	var projectile : Projectile = _projectile_scene.instantiate()
 	player.add_child(projectile)
 	projectile.global_position = player.projectile_spawn.global_position
 	projectile.linear_velocity = Vector3(randf_range(5.0, 50.0), randf_range(-10.0, 10.0), randf_range(-10.0, 10.0))
