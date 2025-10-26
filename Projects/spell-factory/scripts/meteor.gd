e35e244bd79e3d83f7ec3669391bcb83432125e8
Joshua Allen McCoy <dr-jam@users.noreply.github.com>
2025-10-26 09:28:36 -0700

    Removed the unused $Sprite3D @onready variables.

--- Projects/spell-factory/scripts/meteor.gd
+++ Projects/spell-factory/scripts/meteor.gd
@@ -1,9 +1,7 @@
 class_name Meteor
 extends Node3D
 
-@export var speed = 5.0
-
-@onready var sprite:Sprite3D = $Sprite3D
+@export var speed = 5.0 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- Projects/spell-factory/scripts/ultima.gd
+++ Projects/spell-factory/scripts/ultima.gd
@@ -3,8 +3,6 @@ extends Node3D
 
 @export var speed = 5.0
 
-@onready var sprite:Sprite3D = $Sprite3D
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	translate(transform.basis.y * speed * delta)
